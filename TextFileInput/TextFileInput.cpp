bea13efc7bd5d870ea1277ec79da9181f9011d89
Gahee Yang <gahee9006@gmail.com>
2024-06-11 20:39:52 +0900

    파일 이름 입력, 존재 여부 확인

--- TextFileInput/TextFileInput.cpp
+++ TextFileInput/TextFileInput.cpp
@@ -5,12 +5,27 @@ using namespace std;
 
 int main()
 {
-	ifstream input("C:\\Users\\user\\Downloads\\score.txt"); //데이터 읽기 파일 생성 & 열기
+	string filename;
+	cout << "Enter a file name: "; //파일 이름 입력하고
+	cin >> filename;
+
+	//open함수로 전달되는 파일이름은 c문자열이어야 함. string객체로부터 c문자열반환->string클래스의 c_str()함수 호출
+	ifstream input(filename.c_str());
 
 	if (input.fail()) //파일이 존재하는지 여부 확인(open함수 호출후 즉시 fail함수 호출)
 	{
 		cout << "File does not exist." << endl;
 		cout << "Exit program" << endl;
+		return 0;
+	}
+
+	double sum = 0;
+	double number;
+
+	while (input >> number) //input스트림이 number를 변수로 숫자들을 끝날때까지 읽어들임
+	{
+		cout << number << " ";
+		sum += number;
 	}
 
 	string firstname;
@@ -24,16 +39,6 @@ int main()
 
 	input >> firstname >> middlename >> lastname >> score;
 	cout << firstname << " " << middlename << " " << lastname << " " << score << endl;
-
-	double sum = 0;
-	double number;
-
-
-	while (input >> number) //input스트림이 number를 변수로 숫자들을 끝날때까지 읽어들임
-	{
-		cout << number << " "; 
-		sum += number;
-	}
 
 
 	input.close();
